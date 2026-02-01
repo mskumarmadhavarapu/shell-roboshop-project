1f34da9375fd33450b3064fec26f6fe6e1b16e66
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-01 09:18:31 +0530

    roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -26,7 +26,7 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongo Repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$LOGS_FILE
 VALIDATE $? "Installing MongoDB server"
 
 systemctl enable mongod
