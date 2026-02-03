e5f294c67f06496b3fc79e3560877bfcb6c8d998
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-03 22:18:06 +0530

    shell-roboshop

--- mysql.sh
+++ mysql.sh
@@ -23,10 +23,10 @@ VALIDATE(){
     fi
 }
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGS_FILE
 VALIDATE $? "Installing mysql server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGS_FILE
 systemctl start mysqld  
 VALIDATE $? "Enabling and starting mysql"
 
