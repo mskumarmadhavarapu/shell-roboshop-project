a711b4ce36ec02b48f98c23b1dd25a5e5e1fcfd9
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-03 22:25:33 +0530

    shell-roboshop

--- shipping.sh
+++ shipping.sh
@@ -35,7 +35,7 @@ else
     echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "Creating app direcotry"
 
 curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>>$LOGS_FILE
